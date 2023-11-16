3bedcc7fdbb1df2b0449cda72ef116d66fe3096d
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2023-11-17 03:35:33 +0700

    Initial Commit

--- app/src/main/java/com/example/a090_navigationwithdata/MainActivity.kt
+++ app/src/main/java/com/example/a090_navigationwithdata/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello Dunia",
+        text = "Hello Dunia!",
         modifier = modifier
     )
 }
