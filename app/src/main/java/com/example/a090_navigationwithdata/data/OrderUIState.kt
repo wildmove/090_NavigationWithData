317b4c40256546c247d65973b9a316117973d87e
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2023-11-17 03:45:50 +0700

    Add OrderUIState

---
+++ app/src/main/java/com/example/a090_navigationwithdata/data/OrderUIState.kt
@@ -0,0 +1,10 @@
+package com.example.a090_navigationwithdata.data
+
+data class OrderUIState(
+    val nama:String = "",
+    val alamat: String = "",
+    val tlp: String = "",
+    val kuantitas: String = "",
+    val rasaa: String = "",
+    val harga: String = "",
+)
