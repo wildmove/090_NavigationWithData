770241780044e3bebdb147b940e8db74793859a2
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2023-11-17 03:41:27 +0700

    Add DataSource

---
+++ app/src/main/java/com/example/a090_navigationwithdata/data/DataSource.kt
@@ -0,0 +1,13 @@
+package com.example.a090_navigationwithdata.data
+
+import com.example.a090_navigationwithdata.R
+
+object DataSource {
+    val rasa = listOf(
+        R.string.teh_leci,
+        R.string.teh_lemon,
+        R.string.teh_sereh,
+        R.string.teh_original
+
+    )
+}
